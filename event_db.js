25f9ab82379b685ebafffe805e15e6a69a3140fa
Bobby <81536887+Bobby2003@users.noreply.github.com>
2025-10-06 01:55:13 +0800

    111

--- event_db.js
+++ event_db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
+  password: process.env.DB_PASS || '123456',
   database: process.env.DB_NAME || 'charityevents_db',
   waitForConnections: true,
   connectionLimit: 10,
